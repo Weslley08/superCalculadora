3e165dfcf5d011ade5ee1001aea0cfa40b915d17
Weslley08 <83292843+Weslley08@users.noreply.github.com>
2021-06-04 19:41:05 -0300

    Corrigindo erro no settings.json

--- src/circulo/Principal.java
+++ src/circulo/Principal.java
@@ -3,8 +3,6 @@ package circulo;
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 	}
 
 }
--- src/volumes/solidos/Principal.java
+++ src/volumes/solidos/Principal.java
@@ -3,7 +3,6 @@ package volumes.solidos;
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
